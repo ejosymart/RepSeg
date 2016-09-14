5f89d56b9c2b386b055f0d30b0f48f51ba4be04a
ejosymart <ejosymart@gmail.com>
2016-09-14 15:56:04 -0500

    new: demo.R, cpue-internal.R, effort-internal.R

--- demo/demo.R
+++ demo/demo.R
@@ -6,12 +6,16 @@ desembarque = getData(file, type = "landings", toTons = TRUE)
 
 esfuerzo = getData(file, type = "effort")
 
+cpue = getData(file, type = "cpue", toTons = TRUE)
+
 #Tablas
 getTable(desembarque)
-
-getTable(esfuerzo)
 
 getTablaCuota(desembarque, cuota = 10000)
 
 #Gráficos
 plot(desembarque, time = "month", main = "")
+
+plot(esfuerzo, time = "month", main = "")
+
+plot(cpue, time = "month", main = "")
