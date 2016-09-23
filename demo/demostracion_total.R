50886ece1cda18c88a55509af013d3b2a48cdc92
ejosymart <ejosymart@gmail.com>
2016-09-23 09:59:30 -0500

    new demo and .RMD

--- demo/demostracion_total.R
+++ demo/demostracion_total.R
@@ -18,32 +18,9 @@ require(RepSeg)
 #Lectura de datos
 desembarque <- getData(file = file, sp = sp, type = "landings", tipoEsfuerzo = tipoEsfuerzo)
 
-esfuerzo    <- getData(file = file, sp = sp, type = "effort", tipoEsfuerzo = tipoEsfuerzo)
+esfuerzo   <- getData(file = file, sp = sp, type = "effort", tipoEsfuerzo = tipoEsfuerzo)
 
-cpue        <- getData(file = file, sp = sp, type = "cpue", tipoEsfuerzo = tipoEsfuerzo)
-
-# Generación de tablas
-tablaDesembarque <- getTable(desembarque)
-
-tablaDesembarque2 <- getTable2(desembarque)
-
-tablaEsfuerzo    <- getTable(esfuerzo)
-
-tablaCPUE        <- getTable(cpue)
-
-# Generación de gráficos
-plot(desembarque, time = "month")
-
-plot(esfuerzo, time = "month")
-
-plot(cpue, time = "month")
-
-# Obtención de información importante
-summary(desembarque)
-
-summary(esfuerzo)
-
-summary(cpue)
+cpue       <- getData(file = file, sp = sp, type = "cpue", tipoEsfuerzo = tipoEsfuerzo)
 
 # Generación del reporte
 makeReport(desembarque, esfuerzo, cpue, time = "month", cex.axis = 0.5)
